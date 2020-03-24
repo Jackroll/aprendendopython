e9175da97ef154c18993022162f47561bebe438e
Jacson <jeremias.jacson@gmail.com>
2020-03-23 23:49:46 -0300

    atualizao main.py sobre modulos e pacotes

--- exercicios_udemy/Programacao_Procedural/main.py
+++ exercicios_udemy/Programacao_Procedural/main.py
@@ -1,5 +1,5 @@
 from pacotes.calculo_precos import aumento, desconto
-from pacotes.formata import preco
+from pacotes.formata import preco as formatacao #modulo preço importado com um apelido, para a variável preço não sobrescrever por ter o mesmo nome
 
 
 preco = 50.00
@@ -8,3 +8,5 @@ preco_com_desconto = desconto(valor = preco, porcentagem = 50, formata = True)
 
 print(preco_com_aumento)
 print(preco_com_desconto)
+
+print(formatacao.real(100))
